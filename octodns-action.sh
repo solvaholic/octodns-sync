1472dd8ef207839f479bb245f79cdb3534088492
Roger D. Winans <solvaholic@users.noreply.github.com>
2020-03-07 21:12:08 -0500

    Teach octodns-action.sh to do all the things

--- octodns-action.sh
+++ octodns-action.sh
@@ -14,14 +14,25 @@ CONFIG_PATH="${GITHUB_WORKSPACE}/$1"
 PIP_EXTRAS="$2"
 DOIT="$3"
 
+# Activate virtualenv, if it's there.
+if [ -f /env/bin/activate ]; then
+  . /env/bin/activate
+fi
+
 # Change to config directory, so relative paths will work.
 cd "$(dirname "${CONFIG_PATH}")"
 
 # TODO: Implement octodns override, so user can run octodns from a fork.
 
-# Install PIP_EXTRAS.
+# Get octodns, if it's not already there.
+if ! git rev-parse --resolve-git-dir /octodns/.git 2>&1 >/dev/null; then
+  git clone --branch v0.9.9 --depth 1 \
+  https://github.com/github/octodns.git /octodns
+fi
+
+# Install /octodns and PIP_EXTRAS.
 echo "PIP_EXTRAS: ${PIP_EXTRAS}"
-if [ -n "${PIP_EXTRAS}" ]; then pip3 install ${PIP_EXTRAS}; fi
+pip3 install /octodns ${PIP_EXTRAS}
 
 # Run octodns.
 echo "CONFIG_PATH: ${CONFIG_PATH}"
