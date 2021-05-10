a3a38859a376c9fbc6486904ac8b947ad4db5da5
Roger D. Winans <solvaholic@users.noreply.github.com>
2021-05-10 19:59:14 -0400

    Teach install.sh to validate octodns_ref
    and exit early if octodns-sync is already installed.
    
    See #57

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,18 +1,56 @@
 #!/bin/bash
 # Install octodns and dependencies
 
-_ver="$OCTODNS_REF"
+# Exit early if octodns is already installed
+if command -v octodns-sync; then
+  # Hello reader!
+  # This works as a cheap, relatively safe way to respond to #57
+  # If you'd like to improve it, please raise an issue or PR
+  echo "FAIL: It looks like octodns is already installed."
+  exit 1
+fi
 
-if [[ "$_ver" = v* ]]; then
-  # Assume input 'version' is an available release
-  curl -O "https://raw.githubusercontent.com/octodns/octodns/$_ver/requirements.txt"
-  pip install "octodns==${_ver#v}" -r requirements.txt
+# Set some variables
+_ver="$OCTODNS_REF"
+_src="$(mktemp -d)"
+_api="repos/octodns/octodns/git/matching-refs"
+_via=""
+
+# Is _ver a valid pip version or Git ref?
+if pip download -q "octodns==${_ver#v}"; then
+  # _ver is a PyPI version
+  _via=pip
+elif gh api --silent "$_api/tags/$_ver"; then
+  # _ver is a tag in octodns/octodns
+  _via=git
+elif gh api --silent "$_api/heads/$_ver"; then
+  # _ver is a branch in octodns/octodns
+  _via=git
 else
-  # Assume input 'version' is a Git ref in octodns/octodns
-  if git clone --single-branch --branch "$_ver" --no-tags \
-  https://github.com/octodns/octodns.git octodns-src; then
-    pip install ./octodns-src -r ./octodns-src/requirements.txt
+  # _ver is not a branch, tag, or PyPI version
+  echo "FAIL: Didn't find an octodns PyPI version or Git ref '$_ver'."
+  exit 1
+fi
+
+# Install octodns
+if [ "$_via" = "pip" ]; then
+  # Use pip to install octodns
+  _req="https://raw.githubusercontent.com/octodns/octodns/v${_ver#v}/requirements.txt"
+  if curl --output "$_src/requirements.txt" "$_req"; then
+    pip install "octodns==${_ver#v}" -r "$_src/requirements.txt"
   else
+    echo "FAIL: Error downloading requirements.txt from octodns/octodns"
+  fi
+elif [ "$_via" = "git" ]; then
+  # Use Git and then pip to install octodns
+  if git clone --single-branch --branch "$_ver" --no-tags \
+    https://github.com/octodns/octodns.git "$_src"; then
+    pip install "$_src" -r "$_src/requirements.txt"
+  else
+    echo "FAIL: Error cloning octodns/octodns."
     exit 1
   fi
+else
+  echo "FAIL: How did this happen?"
+  exit 1
 fi
