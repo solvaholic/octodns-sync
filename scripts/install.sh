8cafd0c9f0213264abcb9d9149e0a602f8b8cb78
Roger D. Winans <solvaholic@users.noreply.github.com>
2021-05-10 20:10:27 -0400

    Teach install.sh to gh auth login
    and make those variable references consistent .. at least within this
    one file. :grimacing:

--- scripts/install.sh
+++ scripts/install.sh
@@ -16,36 +16,39 @@ _src="$(mktemp -d)"
 _api="repos/octodns/octodns/git/matching-refs"
 _via=""
 
+# Authenticate gh
+echo "${GITHUB_TOKEN}" | gh auth login --with-token
+
 # Is _ver a valid pip version or Git ref?
 if pip download -q "octodns==${_ver#v}"; then
   # _ver is a PyPI version
   _via=pip
-elif gh api --silent "$_api/tags/$_ver"; then
+elif gh api --silent "${_api}/tags/${_ver}"; then
   # _ver is a tag in octodns/octodns
   _via=git
-elif gh api --silent "$_api/heads/$_ver"; then
+elif gh api --silent "${_api}/heads/${_ver}"; then
   # _ver is a branch in octodns/octodns
   _via=git
 else
   # _ver is not a branch, tag, or PyPI version
-  echo "FAIL: Didn't find an octodns PyPI version or Git ref '$_ver'."
+  echo "FAIL: Didn't find an octodns version '${_ver}'."
   exit 1
 fi
 
 # Install octodns
-if [ "$_via" = "pip" ]; then
+if [ "${_via}" = "pip" ]; then
   # Use pip to install octodns
   _req="https://raw.githubusercontent.com/octodns/octodns/v${_ver#v}/requirements.txt"
-  if curl --output "$_src/requirements.txt" "$_req"; then
-    pip install "octodns==${_ver#v}" -r "$_src/requirements.txt"
+  if curl --output "${_src}/requirements.txt" "${_req}"; then
+    pip install "octodns==${_ver#v}" -r "${_src}/requirements.txt"
   else
-    echo "FAIL: Error downloading requirements.txt from octodns/octodns"
+    echo "FAIL: Error downloading requirements.txt."
   fi
-elif [ "$_via" = "git" ]; then
+elif [ "${_via}" = "git" ]; then
   # Use Git and then pip to install octodns
-  if git clone --single-branch --branch "$_ver" --no-tags \
-    https://github.com/octodns/octodns.git "$_src"; then
-    pip install "$_src" -r "$_src/requirements.txt"
+  if git clone --single-branch --branch "${_ver}" --no-tags \
+    https://github.com/octodns/octodns.git "${_src}"; then
+    pip install "${_src}" -r "${_src}/requirements.txt"
   else
     echo "FAIL: Error cloning octodns/octodns."
     exit 1
