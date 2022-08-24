6c2549f32b6380f4c80e8e1556b62d566835b632
kumavis <kumavis@users.noreply.github.com>
2022-08-23 14:59:37 -1000

    comment - throw on status

--- scripts/comment.sh
+++ scripts/comment.sh
@@ -44,4 +44,5 @@ ${_footer}"
   python3 -c "import requests, os, json
 comments_url = json.load(open(os.environ['GITHUB_EVENT_PATH'], 'r'))['pull_request']['comments_url']
 response = requests.post(comments_url, auth=(os.environ['_user'], os.environ['_token']), json={'body':os.environ['_body']}, headers={'user-agent': 'octodns-sync'})
+response.raise_for_status()
 print(response)"
